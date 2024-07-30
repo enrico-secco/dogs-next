40b3583282802422a8d0222885567815a67d1950
Enrico Secco <enricosecco@edu.unifil.br>
2024-07-30 19:02:22 -0300

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
+    <section className="container mainContainer">
       <h1 className="title">Dogs Next</h1>
-    </div>
+    </section >
   );
 }
