7d9a3dc2cfe8be256cc2b2087e8184a31d2d69eb
Enrico Secco <enricosecco@edu.unifil.br>
2024-08-02 13:54:31 -0300

    feat: introduce login components and actions

---
+++ src/components/login/login-form.tsx
@@ -0,0 +1,12 @@
+import login from "@/actions/login";
+
+export default function LoginForm() {
+    return (
+        <>
+            <form action={login}>
+                <input type="text" name="username" placeholder="Usuário" />
+                <input type="password" name="password" placeholder="Senha" />
+            </form>
+        </>
+    )
+}
