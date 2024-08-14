b3004899d22b0e85166f3d78005d94d8ded3ddea
Enrico Secco <enricosecco@edu.unifil.br>
2024-08-14 18:53:40 -0300

    feat: add user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,27 +3,33 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { UserProvider } from "@/context/user-context";
+import userGet from "@/actions/user-get";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
   description: "Social network for dogs",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="pt-BR">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">
-            {children}
-          </main>
-          <Footer />
-        </div>
+        <UserProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">
+              {children}
+            </main>
+            <Footer />
+          </div>
+        </UserProvider>
       </body>
 
     </html>
---
+++ src/context/user-context.tsx
@@ -0,0 +1,29 @@
+import { User } from "@/types/user";
+import React from "react";
+
+type UserContext = {
+    user: User | null;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
+}
+
+const UserContext = React.createContext<UserContext | null>(null);
+
+export const useUser = () => {
+    const context = React.useContext(UserContext);
+
+    if (!context) {
+        throw new Error('useUser must be used within an UserProvider');
+    }
+
+    return context;
+}
+
+export function UserProvider({ children, user }: { children: React.ReactNode, user: User | null }) {
+    const [userState, setUserState] = React.useState<User | null>(user);
+
+    return (
+        <UserContext.Provider value={{ user: userState, setUser: setUserState }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
