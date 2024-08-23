9de654cf84c56fa2f4ea746a02a74701de057fc6
Enrico Secco <enricosecco@edu.unifil.br>
2024-08-23 15:25:12 -0300

    feat: add token validation

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import logout from "@/actions/logout";
+import validateToken from "@/actions/validate-token";
 import { User } from "@/types/user";
 import React from "react";
 
@@ -22,6 +24,16 @@ export const useUser = () => {
 
 export function UserProvider({ children, user }: { children: React.ReactNode, user: User | null }) {
     const [userState, setUserState] = React.useState<User | null>(user);
+
+    React.useEffect(() => {
+        async function validate() {
+            const { ok } = await validateToken();
+            if (!ok) await logout();
+        }
+
+        if (userState) validate();
+
+    }, [userState]);
 
     return (
         <UserContext.Provider value={{ user: userState, setUser: setUserState }}>
